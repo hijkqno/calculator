2bb8378f3004b29f3391d75b3f039da47250540d
hijkqno <hijkqno@gmail.com>
2026-08-29 17:16:56 +0200

    parser: Add bracket counter

---
+++ src/parser.rs
@@ -0,0 +1,36 @@
+use thiserror::Error;
+use crate::lexer::Token;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Bracket counter limit exceeded")]
+    BracketCounterLimitExceeded,
+
+    #[error("Invalid closed bracket")]
+    InvalidClosedBracket,
+
+    #[error("Not all brackets was closed")]
+    NotAllBracketsWasClosed
+}
+
+fn check_brackets(toks: &Vec<Token>) -> Result<(), Error> {
+    let mut count: u8 = 0;
+
+    for t in toks {
+        match t {
+            Token::LeftBracket => {
+                if count == (u8::MAX - 1) {
+                    return Err(Error::BracketCounterLimitExceeded);
+                } count += 1;
+            },
+            Token::RightBracket => {
+                if count == u8::MIN {
+                    return Err(Error::InvalidClosedBracket);
+                } count -= 1;
+            }, _ => {}
+        }
+    }
+    
+    if count == 0 { Ok(()) }
+    else { Err(Error::NotAllBracketsWasClosed) }
+}
